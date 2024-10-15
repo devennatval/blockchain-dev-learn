b4540979be1e03c18f080b8ecc02cc03b9d17a65
Dev <devennatval@gmail.com>
2024-10-15 20:53:26 +0700

    24. Order Book

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import {
   loadAccount, 
   loadTokens,
   loadExchange,
+  loadAllOrders,
   subscribeToEvents
 } from '../store/interactions'
 
@@ -15,6 +16,7 @@ import Navbar from './Navbar'
 import Markets from './Markets'
 import Balance from './Balance'
 import Order from './Order'
+import OrderBook from './OrderBook'
 
 function App() {
   const dispatch = useDispatch()
@@ -48,6 +50,10 @@ function App() {
     const exchangeConfig = config[chainId].exchange
     const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
 
+
+    // Fetch all orders: open, filled, cancelled
+    loadAllOrders(provider, exchange, dispatch)
+
     // Listen to events
     subscribeToEvents(exchange, dispatch)
   }
@@ -79,7 +85,7 @@ function App() {
 
           {/* Trades */}
 
-          {/* OrderBook */}
+          <OrderBook />
 
         </section>
       </main>
