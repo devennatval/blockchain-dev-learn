fef739cbeb385029c0299cce5f902942ee2ce35a
Dev <devennatval@gmail.com>
2024-10-16 23:12:14 +0700

    Amoy Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,6 +11,10 @@ module.exports = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: privateKeys.split(",")
+    },
+    amoy: {
+      apiKey: "jMCwxvF2LD7SBCXGpTy36_IyID8a0sN1", // Replace with your Alchemy API Key.
+      network: Network.MATIC_AMOY, // Replace with your network.
     }
   }
 };
